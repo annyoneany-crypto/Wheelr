18e2f7a449a8a9c2f2c2e152dc8c1bc51ca516d0
annyoneany-crypto <annyone.any@gmail.com>
2026-04-19 10:07:12 +0200

    feat: implement CanonicalService for managing canonical URLs and update app component to utilize it

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,9 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Header } from './feature/header/header';
-import { Router, RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { injectSpeedInsights } from '@vercel/speed-insights';
+import { CanonicalService } from './services/canonical.service';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -17,7 +19,17 @@ import { injectSpeedInsights } from '@vercel/speed-insights';
 })
 export class App implements OnInit {
   router = inject(Router);
-  
+  canonical = inject(CanonicalService);
+
+constructor() {
+  this.router.events.pipe(
+    filter(event => event instanceof NavigationEnd)
+  ).subscribe(() => {
+    // Questo prenderà l'URL corrente automaticamente ad ogni cambio pagina
+    this.canonical.setCanonicalURL(); 
+  }); 
+}
+
   ngOnInit() {
     // Inizializza il monitoraggio
     injectSpeedInsights();
