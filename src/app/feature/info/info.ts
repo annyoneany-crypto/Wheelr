cebca1288c54fdb7871c7314ad77c744d3a45b89
annyoneany-crypto <annyone.any@gmail.com>
2026-04-19 10:15:05 +0200

    feat: add navigation link to Wheel page and enhance live social media examples section

--- src/app/feature/info/info.ts
+++ src/app/feature/info/info.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-info',
-  imports: [ NgOptimizedImage],
+  imports: [NgOptimizedImage, RouterLink],
   templateUrl: './info.html',
   styleUrl: './info.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
